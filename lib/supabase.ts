e5eed4dec050b650393b331435ed372b93b5e8c7
Karasaev Magomed <rostovmen95@gmail.com>
2025-06-30 21:50:55 +0300

    add supabase connect

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
+import {createClient} from "@supabase/supabase-js";
+import {auth} from "@clerk/nextjs/server";
 
-// Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const createSupabaseClient = () => {
+    return createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+            async accessToken() {
+                return ((await auth()).getToken());
+            }
+        }
+    )
+}
